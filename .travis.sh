7aad17d47f82a95e67f6a1b94cfd60ea967b2482
Christian Roth <christian.roth@port17.de>
2015-01-13 19:17:08 +0100

    Test travis building snapshots

---
+++ .travis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+version = `jq -r .version package.json`
+snapshot = `jq -r .snapshot package.json`
+date = `date +"%y%m%d%H%M%S"`
+
+echo "Writing build.info"
+echo "version: $version\nsnapshot: $snapshot\nbuilddate: $date" > build.info
+zip syncany-osx-notifier.app.zip build.info
+
+if [ snapshot == true ]
+  ;then
+    echo "Building snapshot release"
+    mv syncany-osx-notifier.app.zip syncany-osx-notifier-_$version+SNAPSHOT.$date.app.zip
+
+  ;else
+    echo "Building release"
+    mv syncany-osx-notifier.app.zip syncany-osx-notifier_$version.app.zip
+fi
+
